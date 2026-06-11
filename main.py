357c4f1b0ad33b9266bee01b0b8082bc721c1c75
eest1c73 <diego@dominateit.com>
2026-06-11 17:29:54 -0300

    El main.py anterior no les va a servir. A actualizar sus ramas :)
    A actualizar sus ramas.

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 def ejecutar_operacion(nombre_modulo, nombre_funcion, a, b):
-try:
-modulo = __import__(nombre_modulo)
-funcion = getattr(modulo, nombre_funcion)
+    try:
+        modulo = __import__(nombre_modulo)
+        funcion = getattr(modulo, nombre_funcion)
 
-resultado = funcion(a, b)
+        resultado = funcion(a, b)
 
-print(f"{nombre_funcion}({a}, {b}) = {resultado}")
+        print(f"{nombre_funcion}({a}, {b}) = {resultado}")
 
-except ImportError:
-print(f"{nombre_funcion}: módulo '{nombre_modulo}.py' no encontrado")
+    except ImportError:
+        print(f"{nombre_funcion}: módulo '{nombre_modulo}.py' no encontrado")
 
-except AttributeError:
-print(f"{nombre_funcion}: función no encontrada")
+    except AttributeError:
+        print(f"{nombre_funcion}: función no encontrada")
 
-except Exception as e:
-print(f"{nombre_funcion}: error -> {e}")
+    except Exception as e:
+        print(f"{nombre_funcion}: error -> {e}")
+
 
 print("=== CALCULADORA DEL EQUIPO ===")
 print()
@@ -22,5 +23,4 @@ print()
 ejecutar_operacion("suma", "sumar", 10, 5)
 ejecutar_operacion("resta", "restar", 10, 5)
 ejecutar_operacion("multiplicacion", "multiplicar", 10, 5)
-
 ejecutar_operacion("division", "dividir", 10, 5)
